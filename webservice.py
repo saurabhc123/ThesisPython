e65b9edc2c529cee9036e24cfa8b0a993ee23f13
Eric Williamson <ericrw96@vt.edu>
2017-03-04 15:18:17 -0500

    add webservice to give back vectors

---
+++ webservice.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+from word2vec.word2vec import word2vec, avg_feature_vector
+
+app = Flask(__name__)
+model = word2vec.get_model()
+
+
+@app.route("/getvector/<sentence>")
+def get_vector(sentence):
+    words = sentence.split()
+    result = avg_feature_vector(words, model, 300, model.index2word).tolist()
+    return jsonify({"sentence": sentence, "vector": result})
+
+app.run()
