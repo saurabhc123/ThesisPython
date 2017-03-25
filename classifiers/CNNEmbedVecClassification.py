1b1d7c58abf8d890ea16214a34f3421ba73206d3
Saurabh Chakravarty <saurabc@yahoo.com>
2017-03-24 21:42:45 -0400

    CNN solution running now. Now have to integrate it with scala.

---
+++ classifiers/CNNEmbedVecClassification.py
@@ -0,0 +1,118 @@
+import numpy as np
+from keras.layers import Convolution1D, MaxPooling1D, Flatten, Dense
+from keras.models import Sequential
+from nltk import word_tokenize
+
+from classifiers import class_dict
+from utils import ModelNotTrainedException
+import utils.kerasmodel_io as kerasio
+
+# Paper: Yoon Kim, "Convolutional Neural Networks for Sentence Classification," arXiv:1408.5882 (2014).
+#
+# ref: https://gist.github.com/entron/b9bc61a74e7cadeb1fec
+# ref: http://cs231n.github.io/convolutional-networks/
+
+class CNNEmbeddedVecClassifier:
+    def __init__(self,
+                 wvmodel,
+                 n_gram,
+                 classdict = class_dict.classdict,
+                 vecsize=300,
+                 nb_filters=1200,
+                 maxlen=15):
+        self.wvmodel = wvmodel
+        self.classdict = classdict
+        self.n_gram = n_gram
+        self.vecsize = vecsize
+        self.nb_filters = nb_filters
+        self.maxlen = maxlen
+        self.trained = False
+
+    def convert_trainingdata_matrix(self):
+        classlabels = self.classdict.keys()
+        lblidx_dict = dict(zip(classlabels, range(len(classlabels))))
+
+        # tokenize the words, and determine the word length
+        phrases = []
+        indices = []
+        for label in classlabels:
+            for shorttext in self.classdict[label]:
+                category_bucket = [0]*len(classlabels)
+                category_bucket[lblidx_dict[label]] = 1
+                indices.append(category_bucket)
+                phrases.append(word_tokenize(shorttext))
+
+        # store embedded vectors
+        train_embedvec = np.zeros(shape=(len(phrases), self.maxlen, self.vecsize))
+        for i in range(len(phrases)):
+            for j in range(min(self.maxlen, len(phrases[i]))):
+                train_embedvec[i, j] = self.word_to_embedvec(phrases[i][j])
+        indices = np.array(indices, dtype=np.int)
+
+        return classlabels, train_embedvec, indices
+
+
+    def train(self):
+        # convert classdict to training input vectors
+        self.classlabels, train_embedvec, indices = self.convert_trainingdata_matrix()
+
+        # build the deep neural network model
+        model = Sequential()
+        model.add(Convolution1D(nb_filter=self.nb_filters,
+                                filter_length=self.n_gram,
+                                border_mode='valid',
+                                activation='relu',
+                                input_shape=(self.maxlen, self.vecsize)))
+        model.add(MaxPooling1D(pool_length=self.maxlen-self.n_gram+1))
+        model.add(Flatten())
+        model.add(Dense(len(self.classlabels), activation='softmax'))
+        model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+        # train the model
+        model.fit(train_embedvec, indices)
+
+        # flag switch
+        self.model = model
+        self.trained = True
+
+    def savemodel(self, nameprefix):
+        if not self.trained:
+            raise ModelNotTrainedException()
+        kerasio.save_model(nameprefix, self.model)
+        labelfile = open(nameprefix+'_classlabels.txt', 'w')
+        labelfile.write('\n'.join(self.classlabels))
+        labelfile.close()
+
+    def loadmodel(self, nameprefix):
+        self.model = kerasio.load_model(nameprefix)
+        labelfile = open(nameprefix+'_classlabels.txt', 'r')
+        self.classlabels = labelfile.readlines()
+        labelfile.close()
+        self.classlabels = map(lambda s: s.strip(), self.classlabels)
+        self.trained = True
+
+    def word_to_embedvec(self, word):
+        return self.wvmodel[word] if word in self.wvmodel else np.zeros(self.vecsize)
+
+    def shorttext_to_matrix(self, shorttext):
+        tokens = word_tokenize(shorttext)
+        matrix = np.zeros((self.maxlen, self.vecsize))
+        for i in range(min(self.maxlen, len(tokens))):
+            matrix[i] = self.word_to_embedvec(tokens[i])
+        return matrix
+
+    def score(self, shorttext):
+        if not self.trained:
+            raise ModelNotTrainedException()
+
+        # retrieve vector
+        matrix = np.array([self.shorttext_to_matrix(shorttext)])
+
+        # classification using the neural network
+        predictions = self.model.predict(matrix)
+
+        # wrangle output result
+        scoredict = {}
+        for idx, classlabel in zip(range(len(self.classlabels)), self.classlabels):
+            scoredict[classlabel] = predictions[0][idx]
+        return scoredict
--- word2vec/webservice.py
+++ word2vec/webservice.py
@@ -1,11 +1,19 @@
+import json
+
+import decimal
 from flask import Flask, jsonify
 from flask import request
 
+from classifiers import SumWord2VecClassification
+from classifiers.CNNEmbedVecClassification import CNNEmbeddedVecClassifier
+from classifiers.eboladict import eboladict
 from word2vec import word2vec, avg_feature_vector
 
 app = Flask(__name__)
 model = word2vec.get_model()
 file_model = word2vec.get_model_from_file("data/ebola_auxiliary_data_clean.txt")
+sum_classifier = None
+cnn_classifier = None
 
 
 @app.route("/getvector/<sentence>")
@@ -46,5 +54,24 @@ def get_synonyms(word):
 def get_synonyms_file(word):
     similar_words = file_model.most_similar(positive=[word])
     return jsonify({"synonyms": similar_words})
+
+@app.route("/wvsum_score/<word>", methods=['GET'])
+def wvsum_score(word):
+    global sum_classifier
+    if sum_classifier is None:
+        sum_classifier = SumWord2VecClassification.SumEmbeddedVecClassifier(model, classdict=eboladict)
+        sum_classifier.train()
+    score = sum_classifier.score(word)
+    return jsonify({"scores": score})
+
+@app.route("/cnn_score/<word>", methods=['GET'])
+def cnn_score(word):
+    global cnn_classifier
+    if cnn_classifier is None:
+        cnn_classifier = CNNEmbeddedVecClassifier(model,3, classdict=eboladict)
+        cnn_classifier.train()
+    score = {k:float(v) for k,v in cnn_classifier.score(word).iteritems()}
+    return jsonify(score)
+
 
 app.run()
